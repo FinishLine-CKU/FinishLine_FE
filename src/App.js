7501c3d9285691fd9c6d72c3a5f8abd136595e0f
gnawH <sun30126331@gmail.com>
2024-12-28 11:58:29 +0900

    header 레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/footer'
+import Header from './components/header';
 import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
