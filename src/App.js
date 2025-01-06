53977aaf2a7d938cf00a52d9b8b83dea79ff2e3f
Jung Ho <chou_01@kakao.com>
2025-01-06 14:45:02 +0900

    style dev브렌치와 동기화 완료.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignupPage1 from './pages/signupPage1';
+import SignupPage2 from './pages/signupPage2';
 import LoginPage from './pages/loginPage';
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/signupPage1" element={<SignupPage1 />} />
+          <Route path="/signupPage2" element={<SignupPage2 />} />
           <Route path="/loginPage" element={<LoginPage />} /> 
         </Routes>
       </BrowserRouter>
