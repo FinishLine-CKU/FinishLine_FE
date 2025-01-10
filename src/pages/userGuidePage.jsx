a37601d6f7015db55a991995719293da69bb104f
Jung Ho <chou_01@kakao.com>
2025-01-10 22:56:59 +0900

    테스트 로그인 버튼기능 추가

--- src/pages/userGuidePage.jsx
+++ src/pages/userGuidePage.jsx
@@ -1,20 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, css } from "aphrodite";
 import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 import Header from "../components/header";
 import Template from "../components/template";
 import Footer from "../components/footer";
 
-import certification from '../assets/images/certification.png';
-import login from '../assets/images/login.png';
-import subject from '../assets/images/subject.png';
-import requirements from '../assets/images/requirements.png';
-import arrow from '../assets/images/arrow.png';
+import certification from "../assets/images/certification.png";
+import login from "../assets/images/login.png";
+import subject from "../assets/images/subject.png";
+import requirements from "../assets/images/requirements.png";
+import arrow from "../assets/images/arrow.png";
+
+const CommonButton = ({ text, onClick, disabled }) => (
+    <button
+        className={css(styles.commonButton)}
+        onClick={onClick}
+        disabled={disabled}
+    >
+        {text}
+    </button>
+);
+
+CommonButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool,
+};
+
+CommonButton.defaultProps = {
+    disabled: false,
+};
 
 function UserGuidePage() {
     const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [setIsModalOpen] = useState(false);
-    const [setModalContent] = useState('');
+    const [setModalContent] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("userToken");
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
     const openModal = (content) => {
         setModalContent(content);
@@ -23,8 +52,16 @@ function UserGuidePage() {
 
     return (
         <div className={css(styles.userGuideContainer)}>
-            <Header />
-            <Template title="이용 가이드" subtitle={<div>FinishLine을 효과적으로 사용하는 방법을 안내합니다.<br />아래의 안내에 따라 시스템을 사용해보세요.</div>} />
+            <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            <Template 
+                title="이용 가이드" 
+                subtitle={
+                    <div>
+                        FinishLine을 효과적으로 사용하는 방법을 안내합니다.<br />
+                        아래의 안내에 따라 시스템을 사용해보세요.
+                    </div>
+                } 
+            />
 
             <main className={css(styles.mainContent)}>
                 <div className={css(styles.stepsSection)}>
@@ -35,7 +72,7 @@ function UserGuidePage() {
                         <h2 className={css(styles.stepTitle)}>회원가입 및 학생인증</h2>
                         <p className={css(styles.stepDescription)}>
                             대학 포털 ID/PW를 입력하여<br />
-                            학생인증을 하고<br />
+                            학생인증을 하고<br /><br />
                             추가 정보(어학 자격) 및 비밀번호를 <br />
                             설정하여 회원가입을 합니다.
                         </p>
@@ -43,12 +80,11 @@ function UserGuidePage() {
                             학생 인증을 위해 입력한 비밀번호는<br />
                             학생 인증에만 사용되며 저장되지 않습니다.
                         </p>
-                        <button
-                            className={css(styles.signupButton)}
-                            onClick={() => navigate('/signupPage1')}
-                        >
-                            회원가입
-                        </button>
+                        <CommonButton 
+                            text="회원가입"
+                            onClick={() => navigate("/signupPage1")}
+                            disabled={isLoggedIn}
+                        />
                     </div>
 
                     <img src={arrow} alt="화살표" className={css(styles.arrowImage)} />
@@ -62,12 +98,11 @@ function UserGuidePage() {
                             학번과 Finish Line 비밀번호를<br />
                             사용하여 로그인을 합니다.
                         </p>
-                        <button
-                            className={css(styles.loginButton)}
-                            onClick={() => navigate('/loginPage')}
-                        >
-                            로그인
-                        </button>
+                        <CommonButton 
+                            text="로그인"
+                            onClick={() => navigate("/loginPage")}
+                            disabled={isLoggedIn}
+                        />
                     </div>
 
                     <img src={arrow} alt="화살표" className={css(styles.arrowImage)} />
@@ -81,7 +116,7 @@ function UserGuidePage() {
                             가톨릭관동대 포털 종합정보시스템 ＞<br />
                             학적관리 ＞ 학기별 성적조회 및 출력 ＞ <br />
                             년도/학기 설정 및 검색 ＞<br />
-                            인쇄 ＞ PDF 저장<br />
+                            인쇄 ＞ PDF 저장<br /><br />
                             다운받은 PDF를 첨부하여<br />
                             기이수 과목을 등록합니다.
                         </p>
@@ -89,12 +124,10 @@ function UserGuidePage() {
                             첨부한 PDF의 성적은 저장되지 않으며<br />
                             F와 NP 처리된 과목은 반영되지 않습니다.
                         </p>
-                        <button
-                            className={css(styles.subjectButton)}
-                            onClick={() => openModal('졸업요건 검사')}
-                        >
-                            등록하기
-                        </button>
+                        <CommonButton 
+                            text="등록하기"
+                            onClick={() => openModal("졸업요건 검사")}
+                        />
                     </div>
 
                     <img src={arrow} alt="화살표" className={css(styles.arrowImage)} />
@@ -111,14 +144,12 @@ function UserGuidePage() {
                             학점을 계산하여 보여줍니다.
                         </p>
                         <p className={css(styles.notice)}>
-                            기이수 과목 등록을 먼저 진행해주세요.
+                            {isLoggedIn && "기이수 과목 등록을 먼저 진행해주세요."}
                         </p>
-                        <button
-                            className={css(styles.requirementsButton)}
-                            onClick={() => openModal('졸업요건 검사')}
-                        >
-                            결과보기
-                        </button>
+                        <CommonButton 
+                            text="결과보기"
+                            onClick={() => openModal("졸업요건 검사")}
+                        />
                     </div>
                 </div>
             </main>
@@ -135,104 +166,86 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
-        minHeight: '100vh',
+        minHeight: '150vh',
     },
     mainContent: {
-        padding: '20px',
+        padding: '50px',
     },
     stepsSection: {
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
+        justifyContent: 'center',   // 주 축 설정
+        alignItems: 'flex-start',   // 교차 축 설정
         flexWrap: 'nowrap',
-        marginBottom: '50px',
-        gap: '20px',
+        gap: '15px', 
     },
     step: {
         textAlign: 'center',
         alignItems: 'center',
         padding: '20px',
-        width: '200px',
+        width: '210px',  
+        height: '450px', 
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',    // 축 설정
     },
     placeholder: {
-        width: '120px',
-        marginBottom: '15px',
+        width: '200px',
     },
     arrowImage: {
-        marginTop: '60px',
-        width: '30px',
+        marginTop: '70px',
+        width: '60px',
     },
     stepTitle: {
-        fontSize: '20px',
-        fontWeight: 'bold',
-        marginBottom: '8px',
-        color: '#000',
+        fontSize: '25px', 
+        fontWeight: '600',
+        marginBottom: '20px', 
+        whiteSpace: 'nowrap',       // 텍스트가 항상 한 줄로 유지
     },
     stepDescription: {
-        fontSize: '12px',
+        fontSize: '15px', 
         color: '#7A828A',
-        lineHeight: '1.4',
-        marginBottom: '8px',
+        lineHeight: '1.6', 
+        marginBottom: '40px',
+        width: '150%',
+        flex: '1',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start', 
+        alignItems: 'center',
     },
     notice: {
-        fontSize: '11px',
+        fontSize: '15px',
         color: '#006277',
-        marginBottom: '8px',
+        width: '150%',
+        height: '1000px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
     },
-    signupButton: {
-        fontSize: '12px',
+    commonButton: {
+        fontFamily: 'Lato',
+        fontSize: '17px', 
         fontWeight: 'bold',
         color: '#fff',
         backgroundColor: '#2B2A28',
         border: 'none',
-        borderRadius: '5px',
-        padding: '8px 15px',
-        cursor: 'pointer',
-        marginTop: '30px',
+        borderRadius: '8px',
+        padding: '9px 32px', 
+        cursor: 'pointer',              // 커서 "손 모양(👆)"
+        position: 'absolute',
+        top: '620px', 
+        left: '50%',
+        transform: 'translateX(-50%)',  // 가로 가운데 정렬
+        whiteSpace: 'nowrap',           
         ':hover': {
-            backgroundColor: '#2a3d6a',
+            backgroundColor: '#2a3d6a', // 버튼 커서 가져다 대었을때 무슨 색깔로 할지 임시로 파란색
         },
-    },
-    loginButton: {
-        fontSize: '12px',
-        fontWeight: 'bold',
-        color: '#fff',
-        backgroundColor: '#2B2A28',
-        border: 'none',
-        borderRadius: '5px',
-        padding: '8px 15px',
-        cursor: 'pointer',
-        marginTop: '30px',
-        ':hover': {
-            backgroundColor: '#2a3d6a',
-        },
-    },
-    subjectButton: {
-        fontSize: '12px',
-        fontWeight: 'bold',
-        color: '#fff',
-        backgroundColor: '#2B2A28',
-        border: 'none',
-        borderRadius: '5px',
-        padding: '8px 15px',
-        cursor: 'pointer',
-        marginTop: '30px',
-        ':hover': {
-            backgroundColor: '#2a3d6a',
-        },
-    },
-    requirementsButton: {
-        fontSize: '12px',
-        fontWeight: 'bold',
-        color: '#fff',
-        backgroundColor: '#2B2A28',
-        border: 'none',
-        borderRadius: '5px',
-        padding: '8px 15px',
-        cursor: 'pointer',
-        marginTop: '30px',
-        ':hover': {
-            backgroundColor: '#2a3d6a',
+        ':disabled': {
+            backgroundColor: '#d5d3d1',
+            cursor: 'not-allowed',      // 커서 "금지 모양🚫"
+            opacity: '0.7',
         },
     },
 });
