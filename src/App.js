32f1fa220518416ffd124a9cc268c776b4b336e6
Selene-ovo <chou_01@kakao.com>
2025-01-16 16:08:53 +0900

    style 누락된 코드 추가 ->  path=/introPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignupPage1 from './pages/signupPage1';
 import SignupPage2 from './pages/signupPage2';
 import LoginPage from './pages/loginPage';
 import UserGuidePage from './pages/userGuidePage';
+import IntroPage from './pages/introPage';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
         <Routes>
           <Route path="/signupPage1" element={<SignupPage1 />} />
           <Route path="/signupPage2" element={<SignupPage2 />} />
-          <Route path="/loginPage" element={<LoginPage />} /> 
-          <Route path="/userGuidePage" element={<UserGuidePage />} /> 
+          <Route path="/loginPage" element={<LoginPage />} />
+          <Route path="/userGuidePage" element={<UserGuidePage />} />
+          <Route path="/introPage" element={<IntroPage />} />
         </Routes>
       </BrowserRouter>
     </div>
