af73c2cec592fba5132dc6d7ea26620a081460c0
gnawH <sun30126331@gmail.com>
2025-02-26 21:49:59 +0900

    feat 학생 인증 시 Enter 적용

--- src/pages/signupPage1.jsx
+++ src/pages/signupPage1.jsx
@@ -47,6 +47,16 @@ function SignupPage1() {
             };
         };
     };
+    const enterSubmit = (e) => {
+        if(e.key === 'Enter') {
+            if(agree) {
+                studentAuth();
+            } else {
+                alert("이용약관 및 개인정보처리방침에 동의해주세요.");
+            };
+        }
+    };
+
     useEffect(() => {
         if (localStorage.getItem('idToken')) {
             navigate("/userGuidePage");
@@ -150,7 +160,7 @@ function SignupPage1() {
                     <div className={css(styles.certificationContainer)}>
                         <img src={whiteCKULogo} className={css(styles.univLogo)} />
                         <span className={css(styles.guideCertification)}><a href="https://info.cku.ac.kr/haksa/common/loginForm2.jsp" className={css(styles.ckuLoginLink)} target="_blank">가톨릭관동대학교 포털</a> 아이디와 비밀번호를 입력해주세요.</span>
-                        <div className={css(styles.inputContainer)}>
+                        <div className={css(styles.inputContainer)} onKeyDown={enterSubmit}>
                             <label className={css(styles.inputLabel)}>아이디</label>
                             <input className={css(styles.certificationInput)} value={studentId} onChange={(e) => setStudentId(e.target.value)} type="text" placeholder="아이디를 입력하세요."></input>
                             <label className={css(styles.inputLabel)}>비밀번호</label>
