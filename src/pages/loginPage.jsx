95ee12f035b4f6a54dcf53ab1595d5a3ea447f31
Jung Ho <chou_01@kakao.com>
2025-03-03 17:42:35 +0900

    style 디바이스 해상도 레이아웃 조정

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -179,7 +179,7 @@ function LoginPage() {
 
 const styles = StyleSheet.create({
     pageContainer: {
-        minHeight: '122vh',
+        minHeight: '122px',
         display: 'flex',
         flexDirection: 'column',
     },
@@ -193,6 +193,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         textAlign: 'center',
         position: 'relative',
+        marginBottom: '100px',
     },
     loginContent: {
         width: '100%',
